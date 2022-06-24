488b62bbddd6bfb8b8cf11f530cb41e3bf79f191
unknown <felix.rain.liu@gmail.com>
2022-06-24 16:19:00 -0700

    unit test

--- unitTest3/test3.js
+++ unitTest3/test3.js
@@ -2,11 +2,36 @@ var canvas = document.getElementById("A");
 var context = canvas.getContext("2d");
 context.fillStyle = "black";
 context.font = "bold 40px Arial";
-context.fillText("A", (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+context.fillText("C", (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+canvas.myText = "C";
 canvas.addEventListener('click', handleEvent, true);
 
 function handleEvent(param) {
       var canvas = document.getElementById(param.target.id);
+
+      if (!canvas.myText) {
+            return;
+      }
+
+      text = canvas.myText;
       var ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+var text = null;
+
+var canvas = document.getElementById('B');
+canvas.addEventListener('click', handleMove, true);
+
+
+function handleMove(params) {
+      if (text === null) {
+            return;
+      }
+      var canvas = document.getElementById(params.target.id);
+      var ctx = canvas.getContext('2d');
+      ctx.fillStyle = 'black';
+      ctx.font = 'bold 40px Arial';
+      ctx.fillText(text, (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+}
+
