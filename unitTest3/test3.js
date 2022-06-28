8ea0f0bfc2b2eeb974eae90bfe5706e175b4e5e3
unknown <felix.rain.liu@gmail.com>
2022-06-28 11:02:51 -0700

    unit test

--- unitTest3/test3.js
+++ unitTest3/test3.js
@@ -9,11 +9,17 @@ canvas.addEventListener('click', handleEvent, true);
 var canvas = document.getElementById("B");
 canvas.addEventListener('click', handleEvent, true);
 
+var canvas = document.getElementById("C");
+canvas.addEventListener('click', handleEvent, true);
+
+var canvas = document.getElementById("D");
+canvas.addEventListener('click', handleEvent, true);
+
 function handleEvent(param) {
       var canvas = document.getElementById(param.target.id);
       if (canvas.myText) {
             // clear the canvas
-            globalText = canvas.myText;
+            text = canvas.myText;
             var ctx = canvas.getContext('2d');
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             canvas.myText = null;
@@ -22,15 +28,15 @@ function handleEvent(param) {
             var context = canvas.getContext("2d");
             context.fillStyle = "black";
             context.font = "bold 40px Arial";
-            if (globalText) {
-                  context.fillText(globalText, (canvas.width / 2) - 17, (canvas.height / 2) + 8);
-                  canvas.myText = globalText;
-                  globalText = null;
+            if (text) {
+                  context.fillText(text, (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+                  canvas.myText = text;
+                  text = null;
             }
       }
 }
 
-var globalText = null;
+var text = null;
 
 /* use 1 event handler
    use variable members
--- unitTest4/test4.js
+++ unitTest4/test4.js
@@ -2,26 +2,37 @@ var canvas = document.getElementById("A");
 var context = canvas.getContext("2d");
 context.fillStyle = "black";
 context.font = "bold 40px Arial";
-context.fillText("A", (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+context.fillText("C", (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+canvas.myText = 'C';
+canvas.addEventListener('click', handleEvent, true);
+
+var canvas = document.getElementById("B");
+canvas.addEventListener('click', handleEvent, true);
+
+var canvas = document.getElementById("C");
+canvas.addEventListener('click', handleEvent, true);
+
+var canvas = document.getElementById("D");
 canvas.addEventListener('click', handleEvent, true);
 
 function handleEvent(param) {
       var canvas = document.getElementById(param.target.id);
-      var ctx = canvas.getContext('2d');
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      if (canvas.myText) {
+            var ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            globalVar = canvas.myText;
+            canvas.myText = null;
+      } else {
+            var context = canvas.getContext("2d");
+            context.fillStyle = "black";
+            context.font = "bold 40px Arial";
+            if (globalVar) {
+                  context.fillText(globalVar, (canvas.width / 2) - 17, (canvas.height / 2) + 8);
+                  canvas.myText = globalVar;
+                  globalVar = null;
+            }
+      }
 }
 
-var globalVar = canvas.addEventListener('click', handleMove(), true);
-if (document.getElementById('emptySquare2')) {
-      document.getElementById('A');
-      document.getElementById('emptySquare');
-}`
-
-function handleMove() {
-      var canvas = document.getElementById('A');
-      var ctx = canvas.getContext('2d');
-      ctx.fillStyle = "black";
-      ctx.font = "bold 40px Arial";
-      ctx.fillText("A", (canvas.width / 2) - 17, (canvas.height / 2) + 8);
-      canvas.addEventListener('click', handleEvent, true);
-}`
+var globalVar = null;
